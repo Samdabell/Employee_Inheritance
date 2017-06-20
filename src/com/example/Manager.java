65daf3adcb873615c5c504122ddeb34d34406453
Sam DaBell <sam.dabell@gmail.com>
2017-06-20 16:38:55 +0100

    Initial Commit

---
+++ src/com/example/Manager.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Created by Sam on 20/06/2017.
+ */
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(int empId, String name, String ssn, double salary, String deptName){
+        super(empId, name, ssn, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName(){
+        return deptName;
+    }
+}
